f04691bbca1577a806be7ab2136a3b8135686801
Veronica Busiello <veronicab@onetree.com>
2017-10-04 22:22:52 -0300

    Se agrego el arranque del ejercicio 4

---
+++ Practico1/src/practico4/P4Ejercicio4.java
@@ -0,0 +1,17 @@
+package practico4;
+
+public class P4Ejercicio4 {
+    
+    public int MinimoElemArray(int[] arr, int i, int min){
+        int ret = Integer.MAX_VALUE;
+        if (i > arr.length){
+           ret = min; 
+        }
+        else{
+            
+        }
+        return 0;
+    }
+    
+    
+}
